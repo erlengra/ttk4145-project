293831a9f8f91172a4a595625260ae6a0858700c
ingvildforseth <ingvild-f@hotmail.com>
2016-04-09 15:53:57 +0200

    started on the communication module

---
+++ communication/communication.go
@@ -0,0 +1,37 @@
+package communication
+
+import (
+    "../network"
+    "../driver"
+    "encoding/json"
+)
+
+
+type clientMessage struct {
+	direction bool
+	lastPassedFloor int
+	targetFloor int
+}
+
+
+
+
+func CheckError(err error) {
+    if err  != nil {
+        fmt.Println("Error: " , err)
+    }
+}
+
+
+func EncodeClientMessage(message clientMessage) []byte {	
+	dataStream, err := json.Marshal(message)
+	CheckError(err)
+	return dataStream
+}
+
+func DecodeClientMessage(packet network.Packet) clientMessage {	
+     var message ClientData
+     err := json.Unmarshal(b, &message)
+     CheckError(err)
+     return result
+}
