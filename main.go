419f4e71aaf54ed7d68812bbaa84c4871d6009c6
larserikjunge <larserikjunge@gmail.com>
2016-04-10 12:46:31 +0200

    minor changes

--- main.go
+++ main.go
@@ -12,21 +12,23 @@ import (
 //testing
 func main() {
 
-    //To indicate that a process should be the master process, run "go run main.go -master=true"
-    //If it should be a slave elevator just run "go run main.go,", as it defaults to false"
-    isMasterElevator := flag.Bool("master", false, "Set to true to indicate master elevator")
-    flag.Parse()
+	//To indicate that a process should be the master process, run "go run main.go -master=true"
+	//If it should be a slave elevator just run "go run main.go,", as it defaults to false"
+	isMasterElevator := flag.Bool("master", false, "Set to true to indicate master elevator")
+	flag.Parse()
+
+	
 
 
-    switch *isMasterElevator {
-    case true:
-        fmt.Println("This is the master elevator")
-        //Start master elevator routines
-
-    case false:
-        fmt.Println("This is a slave elevator")
-        //start slave elevator routines
-    }
+	switch *isMasterElevator {
+	case true:
+	    fmt.Println("This is the master elevator")
+	    //Start master elevator routines
+	
+	case false:
+	    fmt.Println("This is a slave elevator")
+	    //start slave elevator routines
+	}
 
 
 }
