56da762b0a3e18660fb65ff50d09a0cc4a446a81
larserikjunge <larserikjunge@gmail.com>
2016-04-10 12:26:32 +0200

    Network initialization is almost done, needs testing

--- master/master.go
+++ master/master.go
@@ -11,7 +11,7 @@ import (
 //Modify this so that it gets communication channels as parameters/args.
 //This means that the channels can be used in other functions, after contact has been established.
 //Declaring them in main.go is probably the easiest
-func establishContact(slaveAddress1, slaveAddress2 int) int {
+func masterEstablishContact(slaveAddress1, slaveAddress2 int) int {
 
 	send_channel := make(chan network.Packet)
 	receive_channel := make(chan network.Packet)
@@ -19,8 +19,8 @@ func establishContact(slaveAddress1, slaveAddress2 int) int {
 	err := network.Network_Init(network.localListenPort, network.broadcastListenPort,
         1024, send_channel, receive_channel)
 
-	slave1IP = "129.241.187."+strconv.Itoa(slaveAddress1)
-	slave2IP = "129.241.187."+strconv.Itoa(slaveAddress2)
+	slave1IP := "129.241.187."+strconv.Itoa(slaveAddress1)
+	slave2IP := "129.241.187."+strconv.Itoa(slaveAddress2)
 
 	//numberOfSlavesFound = 0
 	slave1Found = false
@@ -51,6 +51,71 @@ func establishContact(slaveAddress1, slaveAddress2 int) int {
 	return 2
 
 }
+
+
+//Should probably take in channels as arguments as well.
+//Or declare them "globally"...?
+
+//TO-DO: Add functionality to time out if master is not found?
+
+func clientEstablishContact(masterAddress int) {
+
+	send_channel := make(chan network.Packet)
+	receive_channel := make(chan network.Packet)
+
+	err := network.Network_Init(network.localListenPort, network.broadcastListenPort,
+        1024, send_channel, receive_channel)
+
+	//masterDiscovered := false
+	masterIP := "129.241.187."+strconv.Itoa(masterAddress)
+	
+
+	//Waiting for message from Master before continuing
+	for {
+		//time.Sleep(1 * time.Second)
+		rcvMsg := <- receive_channel
+		if rcvMsg.Sender_address == masterIP {break}
+	}
+	//At this point we know that the Master elevator is up
+
+
+	clientMsg := network.Packet{Receiver_address: masterIP+strconv.Itoa(network.localListenPort),
+				    Sender_address = localIP, Data: []byte("Testmsg"), Length:7}
+
+
+	//Sending 10 messages just to be sure, this needs to be changed
+	for i:=1; i<10; i++ {
+		time.Sleep(1* time.Second)
+		send_channel <- clientMsg
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
